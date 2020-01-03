52127fe7f9782f648235e0648448adf37e925a49
ly612322 <1845026488@qq.com>
2020-01-03 14:33:40 +0800

    按需导入

--- src/main.js
+++ src/main.js
@@ -3,19 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import inputFilter from './utils/index'
-import driver from './utils/driver'
 import './assets/style/index.less'
-
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/zh-CN'
 import axios from 'axios'
 import common from './utils/common'
 import qs from 'qs'
 import './permission'
-// import Driver from 'driver.js';
-import 'driver.js/dist/driver.min.css';
-
+import 'default-passive-events'
 import ECharts from 'vue-echarts'
 import 'echarts/lib/chart/bar'
 import 'echarts/lib/component/tooltip'
@@ -28,8 +21,123 @@ import 'echarts/lib/component/legend'
 import 'echarts/lib/component/title.js'
 import animate from 'animate.css'
 Vue.use(animate)
-// Vue.use(Driver)
 Vue.component('chart', ECharts)
+import {
+  Pagination,
+  Dialog,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Switch,
+  Select,
+  Option,
+  OptionGroup,
+  Button,
+  ButtonGroup,
+  Table,
+  TableColumn,
+  DatePicker,
+  TimeSelect,
+  TimePicker,
+  Popover,
+  Tooltip,
+  Breadcrumb,
+  BreadcrumbItem,
+  Form,
+  FormItem,
+  Tabs,
+  TabPane,
+  Icon,
+  Row,
+  Col,
+  Card,
+  Cascader,
+  ColorPicker,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Link,
+  Loading,
+  MessageBox,
+  Message,
+  Notification,
+  Divider,
+  Drawer,
+  Scrollbar
+} from 'element-ui';
+
+Vue.use(Pagination);
+Vue.use(Dialog);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup);
+Vue.use(Input);
+Vue.use(InputNumber);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(RadioButton);
+Vue.use(Checkbox);
+Vue.use(CheckboxButton);
+Vue.use(CheckboxGroup);
+Vue.use(Switch);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(OptionGroup);
+Vue.use(Button);
+Vue.use(ButtonGroup);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(DatePicker);
+Vue.use(TimeSelect);
+Vue.use(TimePicker);
+Vue.use(Popover);
+Vue.use(Tooltip);
+Vue.use(Breadcrumb);
+Vue.use(BreadcrumbItem);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Tabs);
+Vue.use(TabPane);
+Vue.use(Icon);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Card);
+Vue.use(Cascader);
+Vue.use(ColorPicker);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Link);
+Vue.use(Divider);
+Vue.use(Drawer);
+Vue.use(Scrollbar);
+
+
+Vue.use(Loading.directive);
+
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
 
 // import './mock'
 
@@ -51,11 +159,8 @@ if (process.env.NODE_ENV === 'development') { // 开发环境
 Vue.prototype.common = common // 公共方法
 Vue.prototype.$qs = qs
 Vue.prototype.$http = axios
-Vue.use(ElementUI, {
-  locale
-})
+
 Vue.use(inputFilter)
-Vue.use(driver)
 Vue.config.productionTip = false
 
 new Vue({
